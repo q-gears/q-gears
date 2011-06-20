90761288ce732dbf4f145d1bdcd1956cb9ef3b37
bakari@test.targem.local <bakari@test.targem.local>
2011-06-20 19:43:47 +0600

    Add ui files to project.

---
+++ output/data/ui/screens/start_menu.lua
@@ -0,0 +1,31 @@
+ui = {}
+
+
+ui[ "StartMenu" ] = {
+    on_start = function()
+        menu_background:Hide()
+        start_game:Hide()
+        continue:Hide()
+        cursor:Hide()
+    end
+}
+
+
+
+ui[ "StartMenu" ][ "StartMenuBackground" ] = {
+}
+
+
+
+ui[ "StartMenu" ][ "StartMenuBackground" ][ "StartGame" ] = {
+}
+
+
+
+ui[ "StartMenu" ][ "StartMenuBackground" ][ "Continue" ] = {
+}
+
+
+
+ui[ "StartMenu" ][ "StartMenuBackground" ][ "Cursor" ] = {
+}
