9153939df098b8c2698247bea2f4c98f90536f44
paulsapps <github@paulsapps.com>
2015-02-13 06:55:10 +0000

    Update dependencies.sh

--- CIBuild/osx/dependencies.sh
+++ CIBuild/osx/dependencies.sh
@@ -11,10 +11,11 @@ unzip OgreSDK.zip >> OgreSDK_extract.log
 sudo tar xfzC NVIDIA_Cg.tgz /
 unzip OgreDependencies_OSX_libc._20130610.zip >> extract.log
 echo "Setup env vars"
-export OGRE_HOME=${PWD}/../OgreSDK
-export OGRE_SDK=${PWD}/../OgreSDK
-export OIS_HOME=${PWD}/../Dependencies
+export OGRE_HOME="${PWD}/OgreSDK"
+export OGRE_SDK="${PWD}/OgreSDK"
+export OIS_HOME="${PWD}/Dependencies"
 echo "Env vars are $OGRE_SDK and $OIS_HOME"
 # Overwrite broken symlink with actual file
+ls
 cp ${OGRE_SDK}/lib/macosx/Release/Ogre.framework/Versions/1.9.0/Ogre ${OGRE_SDK}/lib/macosx/Release/Ogre.framework/Ogre
 cp ${OGRE_SDK}/lib/macosx/Release/OgreOverlay.framework/Versions/1.9.0/OgreOverlay ${OGRE_SDK}/lib/macosx/Release/OgreOverlay.framework/OgreOverlay
