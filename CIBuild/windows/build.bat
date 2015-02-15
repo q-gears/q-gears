2e06d8af5658d5d15fbdb33afcae0890731687ec
paul <paul@build.com>
2015-02-15 15:26:09 +0000

    Fix windows unit tests

--- CIBuild/windows/build.bat
+++ CIBuild/windows/build.bat
@@ -11,6 +11,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 set QTDIR=C:\qt-everywhere-opensource-src-4.8.6_32
 set OGRE_HOME=C:\Ogre\Build\sdk
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+set path=%path%;C:\boost_1_55_0_32\lib32-msvc-12.0;C:\qt-everywhere-opensource-src-4.8.6_32\bin\release;C:\Ogre\Build\bin\relwithdebinfo
 
 cmake .. -T v120_xp -DZLIB_INCLUDE_DIR=C:/zlib-1.2.8 -DBOOST_ROOT=C:\boost_1_55_0_32 -DBOOST_LIBRARYDIR=C:\boost_1_55_0_32\lib32-msvc-12.0 -DOGRE_DIR=C:\Ogre\Build\sdk -DOIS_HOME=C:\Ogre\Dependencies\Build\ogredeps -DZLIB_LIBRARY=C:/zlib-1.2.8/contrib/vstudio/vc11/x86/ZlibDllReleaseWithoutAsm/zlibwapi.lib
 if %errorlevel% neq 0 exit /b %errorlevel%
