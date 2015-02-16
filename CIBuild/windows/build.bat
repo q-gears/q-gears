0b57580c390888f78d5310a1e239cc000686b28e
paulsapps <github@paulsapps.com>
2015-02-16 01:09:40 +0000

    fix windows 32bit build

--- CIBuild/windows/build.bat
+++ CIBuild/windows/build.bat
@@ -21,7 +21,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cpack -C "Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-set path=%path%;C:\boost_1_55_0_32\lib32-msvc-12.0;C:\qt-everywhere-opensource-src-4.8.6_32\bin\release;C:\Ogre\Build\bin\relwithdebinf
+set path=%path%;C:\boost_1_55_0_32\lib32-msvc-12.0;C:\qt-everywhere-opensource-src-4.8.6_32\bin\release;C:\Ogre\Build\bin\relwithdebinfo
 ctest -C "Release" --verbose
 if %errorlevel% neq 0 exit /b %errorlevel%
 
